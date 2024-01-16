0ef61b6aaeb4c367e2392e8b3f8e2698ebc19574
samueljcatania <samueljcatania@gmail.com>
2024-01-15 23:29:38 -0500

    Added dark theme and removed boilerplate NextJS code.

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { Button } from "@/src/components/ui/button"
+
+export default function Home() {
+  return (
+    <main>
+
+    </main>
+  )
+}
---
+++ src/app/random/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p>Random Page</p>;
+}
